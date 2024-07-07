dd6f59d31852e887a059016bafd4847f213bf537
CodeKiro <kirofiniello@outlook.it>
2024-07-07 23:25:46 +0200

    inizio a definire le classi e i metodi con le relative appartenenze , e l'uso del costruttore in product

---
+++ classes/Product.php
@@ -0,0 +1,39 @@
+<?php 
+class Prdouct{
+    /** @var [Category]*/
+    private $category;
+
+    private $name;
+    private $brand;
+    private $price;
+    private $imageUrl;
+    private $isAvailable;
+
+    public function __construct($category, $name, $brand, $price, $imageUrl, $isAvailable){
+        $this->category = $category;
+        $this->name = $name;
+        $this->brand = $brand;
+        $this->price = $price;
+        $this->imageUrl = $imageUrl;
+        $this->isAvailable =$isAvailable;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+    public function getPrice(){
+        return $this->price;
+    }
+    public function getImageUrl(){
+        return $this->imageUrl;
+    }
+    public function getIsAvailable(){
+        return $this->isAvailable;
+    }
+    public function getBrand(){
+        return $this->brand;
+    }
+    public function getCategory(){
+        return $this->category;
+    }
+}
